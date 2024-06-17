494884d934b459454f52bf2e1ab3638429cb45e8
DoHyun_Kwon <tlstoddk99@gmail.com>
2024-06-17 14:16:44 +0900

    트랙 제약 없앰 (실행 가능)

--- casadi_mpc_ex.py
+++ casadi_mpc_ex.py
@@ -2,6 +2,7 @@ import casadi as ca
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
 def main():
     # 데이터 로드
     N = 100  # 제어 구간의 수
@@ -10,26 +11,36 @@ def main():
     df_1 = pd.read_csv(center_w, sep=",", header=None)
     x_c = df_1[0].values
     y_c = df_1[1].values
+    x_c = ca.DM(x_c)
+    y_c = ca.DM(y_c)
 
     left_w = "/home/a/mpc_homework/map/left_waypoints.csv"
     df_2 = pd.read_csv(left_w, sep=",", header=None)
     x_l = df_2[0].values
     y_l = df_2[1].values
+    x_l = ca.DM(x_l)
+    y_l = ca.DM(y_l)
 
     right_w = "/home/a/mpc_homework/map/right_waypoints.csv"
     df_3 = pd.read_csv(right_w, sep=",", header=None)
     x_r = df_3[0].values
     y_r = df_3[1].values
+    x_r = ca.DM(x_r)
+    y_r = ca.DM(y_r)
 
     spline = "/home/a/mpc_homework/map/center_spline_derivatives.csv"
     df_4 = pd.read_csv(spline, sep=",", header=None)
     s_x = df_4[0].values
     s_y = df_4[1].values
+    s_x = ca.DM(s_x)
+    s_y = ca.DM(s_y)
 
     width = "/home/a/mpc_homework/map/track_widths.csv"
     df_5 = pd.read_csv(width, sep=",", header=None)
     w_l = df_5[0].values
     w_r = df_5[1].values
+    w_l = ca.DM(w_l)
+    w_r = ca.DM(w_r)
 
     # 최적화 문제 설정
     opti = ca.Opti()
@@ -42,7 +53,15 @@ def main():
 
     # 결정 변수
     X = opti.variable(7, N + 1)
-    p_x, p_y, yaw, v_x, v_y, omega, theta = X[0, :], X[1, :], X[2, :], X[3, :], X[4, :], X[5, :], X[6, :]
+    p_x, p_y, yaw, v_x, v_y, omega, theta = (
+        X[0, :],
+        X[1, :],
+        X[2, :],
+        X[3, :],
+        X[4, :],
+        X[5, :],
+        X[6, :],
+    )
     U = opti.variable(3, N)
     T = opti.variable()
 
@@ -60,8 +79,10 @@ def main():
         Fr_x = (Cm1 - Cm2 * x[3]) * u[0] - Cr - Cd * x[3] ** 2
 
         dxdt = ca.vertcat(
-            x[3] * ca.cos(x[2]) - x[4] * ca.sin(x[2]),  # dx/dt = vx * cos(yaw) - vy * sin(yaw)
-            x[3] * ca.sin(x[2]) + x[4] * ca.cos(x[2]),  # dy/dt = vx * sin(yaw) + vy * cos(yaw)
+            x[3] * ca.cos(x[2])
+            - x[4] * ca.sin(x[2]),  # dx/dt = vx * cos(yaw) - vy * sin(yaw)
+            x[3] * ca.sin(x[2])
+            + x[4] * ca.cos(x[2]),  # dy/dt = vx * sin(yaw) + vy * cos(yaw)
             x[5],  # dyaw/dt = omega
             (Fr_x - Ff_y * ca.sin(u[1]) + m * x[4] * x[5]) / m,  # dvx/dt
             (Fr_y + Ff_y * ca.cos(u[1])) / m,  # dvy/dt
@@ -96,23 +117,25 @@ def main():
 
         return min_dist, min_idx
 
-    # 경로 제약 조건
-    for k in range(N + 1):
-        distance, idx = closest_idx(p_x[k], p_y[k])
-        opti.subject_to(opti.bounded(0, distance, 0.7))
+    # # 경로 제약 조건
+    # for k in range(N + 1):
+    #     distance, idx = closest_idx(p_x[k], p_y[k])
+    #     opti.subject_to(opti.bounded(0, distance, 3))
 
     # 경계 조건
-    opti.subject_to(p_x[0] == x_c[0])
+
+    opti.subject_to(p_x[0] == x_c[0])  # 초기 위치
     opti.subject_to(p_y[0] == y_c[0])
-    opti.subject_to(yaw[0] == 0)
-    opti.subject_to(v_x[0] == 0.1)
+    opti.subject_to(yaw[0] == 0)  # 초기 각도
+    opti.subject_to(v_x[0] == 0.1)  # 초기 속도
     opti.subject_to(v_y[0] == 0)
     opti.subject_to(omega[0] == 0)
     opti.subject_to(theta[0] == 0)
 
-    opti.subject_to(p_x[-1] == x_c[-1])
-    opti.subject_to(p_y[-1] == y_c[-1])
-    opti.subject_to(opti.bounded(-5, U[0, :], 5))
+    opti.subject_to(p_x[-1] == x_c[500])  # 최종 위치
+    opti.subject_to(p_y[-1] == y_c[500])
+
+    opti.subject_to(opti.bounded(0, U[0, :], 5))
     opti.subject_to(opti.bounded(-0.4, U[1, :], 0.4))
     opti.subject_to(T >= 0)
 
@@ -127,7 +150,7 @@ def main():
     opti.set_initial(U[0, :], 0)  # throttle
     opti.set_initial(U[1, :], 0)  # steering
     opti.set_initial(U[2, :], 0)  # theta
-    opti.set_initial(T, 0)  # Time
+    opti.set_initial(T, 10)  # Time
 
     # NLP 문제 풀기
     opti.solver("ipopt")
@@ -145,14 +168,15 @@ def main():
     plt.xlabel("x")
     plt.ylabel("y")
     plt.legend()
-    plt.plot(x_c, y_c, "g", label="centerline")
+    plt.plot(x_c, y_c, "g--", label="centerline")
     plt.plot(x_l, y_l, "r", label="left boundary")
     plt.plot(x_r, y_r, "r", label="right boundary")
     plt.show()
 
     plt.subplot(2, 1, 1)
-    plt.plot(sol.value(v_x), "b", label="v_x")
-    plt.plot(sol.value(v_y), "r", label="v_y")
+    plt.plot(sol.value(yaw), label="yaw")
+    plt.plot(sol.value(v_x), label="v_x")
+    plt.plot(sol.value(v_y), label="v_y")
     plt.xlabel("Time step")
     plt.ylabel("State")
     plt.legend()
@@ -164,6 +188,7 @@ def main():
     plt.ylabel("Control input")
     plt.legend()
     plt.show()
+
 
 if __name__ == "__main__":
     main()
